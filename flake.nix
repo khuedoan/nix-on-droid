14b8ac4081d75493cfa2c37f35e6bf5057dfbe10
Khue Doan <mail@khuedoan.com>
2025-07-27 15:13:19 +0700

    style: rename to configuration.nix

--- flake.nix
+++ flake.nix
@@ -10,7 +10,9 @@
   outputs = { self, nixpkgs, nix-on-droid }: {
     nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
       pkgs = import nixpkgs { system = "aarch64-linux"; };
-      modules = [ ./nix-on-droid.nix ];
+      modules = [
+        ./configuration.nix
+      ];
     };
   };
 }
