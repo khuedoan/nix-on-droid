959d28d182c5468fd53fa1e22ba72ae05d68bbcf
Khue Doan <mail@khuedoan.com>
2025-07-27 15:06:07 +0700

    feat: Android integration and custom theme
    Black on white for E Ink devices.

--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -1,46 +1,33 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Simply install just the packages
-  environment.packages = with pkgs; [
-    # User-facing stuff that you really really want to have
-    vim # or some other editor, e.g. nano or neovim
-neovim
-git
-tmux
-openssh
+  environment = {
+    packages = with pkgs; [
+      git
+      neovim
+      openssh
+      tmux
+    ];
+    etcBackupExtension = ".bak";
+  };
 
-    # Some common stuff that people expect to have
-    #procps
-    #killall
-    #diffutils
-    #findutils
-    #utillinux
-    #tzdata
-    #hostname
-    #man
-    #gnugrep
-    #gnupg
-    #gnused
-    #gnutar
-    #bzip2
-    #gzip
-    #xz
-    #zip
-    #unzip
-  ];
-
-  # Backup etc files instead of failing to activate generation if a file already exists in /etc
-  environment.etcBackupExtension = ".bak";
-
-  # Read the changelog before changing this value
-  system.stateVersion = "24.05";
-
-  # Set up nix for flakes
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
 
-  # Set your time zone
-  #time.timeZone = "Europe/Berlin";
+  time.timeZone = "Asia/Ho_Chi_Minh";
+
+  android-integration = {
+    termux-reload-settings.enable = true;
+    termux-setup-storage.enable = true;
+  };
+
+  terminal.colors = {
+    background = "#FFFFFF";
+    foreground = "#000000";
+    cursor = "#000000";
+  };
+
+  # Read the changelog before changing this value
+  system.stateVersion = "24.05";
 }
